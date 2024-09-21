16676dce4d15061dfd7f4fd81bc3737f42b0e0c2
Vladislav Bakushkin <bakushkinv@yandex.ru>
2024-09-21 14:19:15 +0300

    init project

---
+++ user-data-service/src/test/java/ru/bakushkin/userdataservice/UserDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bakushkin.userdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ user-manager-app/src/test/java/ru/bakushkin/usermanager/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bakushkin.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
